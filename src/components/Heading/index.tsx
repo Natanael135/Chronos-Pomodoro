9446f57c336bc084c776956d59834f1e2f84d936
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-04-03 09:50:55 -0300

    feat: implementando componente heading na aplicação

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,9 @@
+import Styles from "./styles.module.css";
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export default function Heading({ children }: HeadingProps) {
+  return <h1 className={Styles.heading}>{children}</h1>;
+}
